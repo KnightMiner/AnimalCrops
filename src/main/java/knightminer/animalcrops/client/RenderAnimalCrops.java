03cb7bd57d08997e53aa610f1af38079ecf895a7
KnightMiner <knightminer4@gmail.com>
2018-07-13 21:40:34 -0500

    Improve crop model: instead of translating the entity we shrink it, and we also now tint it green slightly

--- src/main/java/knightminer/animalcrops/client/RenderAnimalCrops.java
+++ src/main/java/knightminer/animalcrops/client/RenderAnimalCrops.java
@@ -11,17 +11,28 @@ public class RenderAnimalCrops extends TileEntitySpecialRenderer<TileAnimalCrops
 	private static final Minecraft mc = Minecraft.getMinecraft();
     @Override
 	public void render(TileAnimalCrops te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
+    	int meta = te.getBlockMetadata();
+    	if(meta == 0) {
+    		return;
+    	}
     	EntityAgeable entity = te.getEntity();
     	if(entity == null) {
     		return;
     	}
 
     	// its pretty easy, just draw the entity
+    	GlStateManager.pushMatrix();
 		int brightness = mc.world.getCombinedLight(te.getPos(), 0);
         int j = brightness & 0xFFFF;
         int k = brightness >> 0x10 & 0xFFFF;
         OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, j, k);
-        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-    	mc.getRenderManager().doRenderEntity(entity, x + 0.5, y - ((7 - te.getBlockMetadata()) / 8f), z + 0.5, entity.rotationYaw, 0, false);
+        GlStateManager.color(0.75F, 1.0F, 0.75F, 1.0F);
+        GlStateManager.translate(x + 0.5, y, z + 0.5);
+        if(meta < 7) {
+            float scale = meta / 7f;
+            GlStateManager.scale(scale, scale, scale);
+        }
+    	mc.getRenderManager().doRenderEntity(entity, 0, 0, 0, entity.rotationYaw, 0, false);
+    	GlStateManager.popMatrix();
     }
 }
