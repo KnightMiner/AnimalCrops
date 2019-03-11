2be5ee533f33329915b0ccaf2b68226a958fb627
KnightMiner <knightminer4@gmail.com>
2019-03-10 19:53:28 -0500

    Add a sound effect when planting animal crops

--- src/main/java/knightminer/animalcrops/items/ItemAnimalSeeds.java
+++ src/main/java/knightminer/animalcrops/items/ItemAnimalSeeds.java
@@ -11,6 +11,7 @@ import net.minecraft.entity.EntityList;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemSeeds;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -19,6 +20,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.translation.I18n;
 import net.minecraft.world.World;
@@ -98,6 +100,7 @@ public class ItemAnimalSeeds extends ItemSeeds {
             }
 
             stack.shrink(1);
+            world.playSound(player, pos, SoundEvents.BLOCK_GRASS_PLACE, SoundCategory.BLOCKS, 1.0F, 1.0F);
             return EnumActionResult.SUCCESS;
         }
 
